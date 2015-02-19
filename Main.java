40e4a7af1cdfc7f2d42f3c4208912269283c33f1
thedramaticginger <tannero@g.clemson.edu>
2015-02-18 22:02:13 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+package IssueClient.src;
+
+public class Main {
+	public static void main(String args[]) {		
+		//myFrame mf = new myFrame();
+		ConfigurationDlg dlg = new ConfigurationDlg();
+		dlg.setVisible(true);
+	}
+}
